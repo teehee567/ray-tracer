dd8ed93419a9c96dd88324f16e03f998a5a001b1
Alex <81747218+teehee567@users.noreply.github.com>
2024-09-20 23:08:57 +1000

    alskdjf

--- tracer/src/mesh.rs
+++ tracer/src/mesh.rs
@@ -1,4 +1,4 @@
-use nalgebra::{Point2, Point3, Vector3};
+use nalgebra::{Normed, Point2, Point3, Vector3};
 
 use crate::{
     aabb::AABB,
@@ -23,12 +23,36 @@ pub struct Triangle {
     pub aabb: AABB, //FIX: get rid of aabb here, make it calculated??? test differences
 }
 
+// PBRT "Unlike the other shapes so far, pbrt provides a stand-alone triangle intersection
+// function that takes a ray and the three triangle vertices directly. Having this
+// functionality available without needing to instantiate both a Triangle and a
+// TriangleMesh in order to do a ray–triangle intersection test is helpful in a few
+// other parts of the system"
+pub struct TriangleIntersection {
+
+}
+
+pub fn intersect_triange(
+    ray: &Ray,
+    ray_t: f32,
+    p0: Point3<f32>,
+    p1: Point3<f32>,
+    p2: Point3<f32>,
+) -> Option<TriangleIntersection> {
+    if Vector3::cross(&(p2 - p0), &(p1 - p0)).norm_squared() == 0. {
+        return None;
+    }
+
+
+    todo!()
+}
+
 impl Triangle {
     pub fn new(mesh: &TriangleMesh, mesh_i: i32, tri_i: i32) -> Self {
-        let v = &mesh.vertex_i[(tri_i * 3)..(tri_i * 3 + 3)];
-        let p0 = mesh.p[v[0]];
-        let p1 = mesh.p[v[1]];
-        let p2 = mesh.p[v[2]];
+        let v = &mesh.vertex_i[(tri_i as usize * 3)..(tri_i as usize * 3 + 3)];
+        let p0 = mesh.p[v[0] as usize];
+        let p1 = mesh.p[v[1] as usize];
+        let p2 = mesh.p[v[2] as usize];
         let aabb = AABB::new(p0, p1).union(p2);
 
         Self {
@@ -44,5 +68,7 @@ impl Hittable for Triangle {
         todo!()
     }
 
-    fn bounding_box(&self) -> &AABB {}
+    fn bounding_box(&self) -> &AABB {
+        &self.aabb
+    }
 }
