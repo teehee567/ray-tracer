efb131bc69910a797cf51782cf1ebe9d625c6a88
teehee567 <81747218+teehee567@users.noreply.github.com>
2025-01-31 15:29:00 +1100

    asdlkf

---
+++ bruh/compile_shaders.bat
@@ -0,0 +1,14 @@
+:: Check if VULKAN_SDK environment variable is set
+if "%VULKAN_SDK%"=="" (
+    echo Error: VULKAN_SDK environment variable is not set. Please set it to the Vulkan SDK installation path.
+    pause
+    exit /b 1
+)
+echo %VULKAN_SDK%
+
+:: Set GLSLC path and compile shaders
+set GLSLC_PATH=%VULKAN_SDK%\Bin\glslc.exe
+
+%GLSLC_PATH% ./shaders/main.vert -o ./shaders/main.vert.spv
+%GLSLC_PATH% ./shaders/main.frag -o ./shaders/main.frag.spv
+
