e2a4674083203007ef4d53c1bc7b883b02bf2f09
Alex <81747218+teehee567@users.noreply.github.com>
2025-02-09 18:20:26 +1100

    aldsf

---
+++ bruh/compile_shaders.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+if [ -z "$VULKAN_SDK" ]; then
+    echo "Error: VULKAN_SDK environment variable is not set. Please set it to the Vulkan SDK installation path."
+    exit 1
+fi
+
+echo "$VULKAN_SDK"
+
+# Set GLSLC path (note: on macOS, the executable is typically named "glslc" without the .exe extension)
+GLSLC_PATH="$VULKAN_SDK/bin/glslc"
+
+# Compile the shader
+"$GLSLC_PATH" ./shaders/main.comp -o ./shaders/main.comp.spv
