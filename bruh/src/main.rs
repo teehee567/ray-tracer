613ad9ffc4003664d85d569b620f1c8e50df0dd0
Alex <81747218+teehee567@users.noreply.github.com>
2025-02-13 21:54:02 +1100

    hi

--- bruh/src/main.rs
+++ bruh/src/main.rs
@@ -17,6 +17,7 @@ use accelerators::Primitive;
 use anyhow::{anyhow, Result};
 use glam::{Mat4, UVec2, Vec2, Vec3, Vec4};
 use log::info;
+use scene::Scene;
 use serde::{Deserialize, Serialize};
 use vulkan::accumulate_image::{create_image, transition_image_layout};
 use vulkan::buffers::{create_shader_buffers, create_uniform_buffer};
@@ -31,7 +32,6 @@ use vulkan::logical_device::create_logical_device;
 use vulkan::physical_device::{pick_physical_device, SuitabilityError};
 use vulkan::pipeline::{create_compute_pipeline, create_render_pass};
 use vulkan::sampler::create_sampler;
-use vulkan::scene::Scene;
 use vulkan::swapchain::{create_swapchain, create_swapchain_image_views};
 use vulkan::sync_objects::create_sync_objects;
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
@@ -47,8 +47,12 @@ use vulkanalia::vk::ExtDebugUtilsExtension;
 use vulkanalia::vk::KhrSurfaceExtension;
 use vulkanalia::vk::KhrSwapchainExtension;
 
+mod scene;
 mod vulkan;
+mod types;
+pub use types::*;
 mod accelerators;
+
 
 /// Whether the validation layers should be enabled.
 // const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
@@ -81,6 +85,7 @@ fn main() -> Result<()> {
     print_size!(Vertex);
 
     let scene = Scene::new("./fancy.yaml")?;
+    // let scene = Scene::from_gltf("./low_poly_lake_scene/scene.gltf")?;
 
     let event_loop = EventLoop::new()?;
     let window = WindowBuilder::new()
@@ -109,7 +114,7 @@ fn main() -> Result<()> {
 
         app.data.scene.write_buffers(mapped_ptr);
 
-        println!("sizes: {}, {}, {}", sizes.0, sizes.1, sizes.2);
+        println!("sizes: bvh({}), mat({}), tri({})", sizes.0, sizes.1, sizes.2);
         println!("Total memory: {}", total_size);
 
     }
@@ -479,214 +484,5 @@ impl SwapchainSupport {
             present_modes: instance
                 .get_physical_device_surface_present_modes_khr(physical_device, data.surface)?,
         })
-    }
-}
-
-#[repr(C)]
-#[repr(align(64))]
-#[derive(Copy, Clone, Debug, Default)]
-struct CameraBufferObject {
-    resolution: AlignedUVec2,
-    view_port_uv: AlignedVec2,
-    focal_length: Alignedf32,
-    focus_distance: Alignedf32,
-    aperture_radius: Alignedf32,
-    time: Alignedu32,
-    location: AlignedVec3,
-    rotation: AlignedMat4,
-}
-
-#[repr(C)]
-#[repr(align(16))]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct AlignedVec3(pub Vec3);
-impl AlignedVec3 {
-    pub fn new(x: f32, y: f32, z: f32) -> Self {
-        Self(Vec3::new(x, y, z))
-    }
-}
-
-// impl Serialize for AlignedVec3 {
-//     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-//     where
-//         S: serde::Serializer {
-//         let mut state = serializer.serialize_struct("Vec3", 3)?;
-//         state.serialize_field("x", &self.0.x)?;
-//         state.serialize_field("y", &self.0.y)?;
-//         state.serialize_field("z", &self.0.z)?;
-//         state.end()
-//     }
-// }
-
-impl From<[f32; 3]> for AlignedVec3 {
-    fn from(value: [f32; 3]) -> Self {
-        AlignedVec3::new(value[0], value[1], value[2])
-    }
-}
-
-#[repr(C)]
-#[repr(align(16))]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct AlignedMat4(pub Mat4);
-
-#[repr(C)]
-#[repr(align(16))]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct AlignedVec4(pub Vec4);
-impl AlignedVec4 {
-    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
-        Self(Vec4::new(x, y, z, w))
-    }
-
-    pub fn default() -> Self {
-        Self(Vec4::new(0., 0., 0., 0.))
-    }
-}
-
-#[repr(C)]
-#[repr(align(4))]
-#[derive(Copy, Clone, Debug, Default, Serialize)]
-pub struct Alignedf32(pub f32);
-
-#[repr(C)]
-#[repr(align(8))]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct AlignedVec2(pub Vec2);
-
-#[repr(C)]
-#[repr(align(8))]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct AlignedUVec2(pub UVec2);
-
-#[repr(C)]
-#[repr(align(4))]
-#[derive(Copy, Clone, Debug, Default, Serialize)]
-pub struct Alignedu32(pub u32);
-
-#[repr(C)]
-#[repr(align(4))]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct AlignedBool(pub bool);
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct Material {
-    base_colour: AlignedVec3,
-    emission: AlignedVec3,
-    reflectivity: Alignedf32,
-    roughness: Alignedf32,
-    ior: Alignedf32,
-    is_glass: Alignedu32,
-    shade_smooth: Alignedu32,
-    motion_blur: AlignedVec3,
-}
-
-#[repr(C)]
-#[repr(align(16))]
-#[derive(Copy, Clone, Debug, Default)]
-pub struct Triangle {
-    pub material_index: Alignedu32,
-    pub is_sphere: Alignedu32,
-    pub vertices: [AlignedVec3; 3],
-    pub normals: [AlignedVec3; 3],
-}
-
-
-impl Primitive for Triangle {
-    fn centroid(&self) -> Vec3 {
-        (self.vertices[0].0 + self.vertices[1].0 + self.vertices[2].0) * 0.3333333f32
-    }
-
-    fn aabb(&self) -> accelerators::aabb::AABB {
-        accelerators::aabb::AABB::new(self.vertices[0].0, self.vertices[1].0).grow(self.vertices[2].0)
-    }
-}
-
-impl Triangle {
-    pub fn min_bound(&self) -> Vec3 {
-        if self.is_sphere.0 == 1 {
-            let radius = self.vertices[1].0.x;
-            self.vertices[0].0 - Vec3::splat(radius)
-        } else {
-            Vec3 {
-                x: self.vertices.iter().map(|v| v.0.x).fold(f32::INFINITY, f32::min),
-                y: self.vertices.iter().map(|v| v.0.y).fold(f32::INFINITY, f32::min),
-                z: self.vertices.iter().map(|v| v.0.z).fold(f32::INFINITY, f32::min),
-            }
-        }
-    }
-
-    pub fn max_bound(&self) -> Vec3 {
-        if self.is_sphere.0 == 1 {
-            let radius = self.vertices[1].0.x;
-            self.vertices[0].0 + Vec3::splat(radius)
-        } else {
-            Vec3 {
-                x: self.vertices.iter().map(|v| v.0.x).fold(f32::NEG_INFINITY, f32::max),
-                y: self.vertices.iter().map(|v| v.0.y).fold(f32::NEG_INFINITY, f32::max),
-                z: self.vertices.iter().map(|v| v.0.z).fold(f32::NEG_INFINITY, f32::max),
-            }
-        }
-    }
-}
-
-#[repr(C)]
-#[derive(Clone, Default)]
-struct SceneComponents {
-    camera: CameraBufferObject,
-    bvh: Vec<BvhNode>,
-    materials: Vec<Material>,
-    triangles: Vec<Triangle>
-}
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-struct Sphere {
-    center: AlignedVec3,
-    radius: Alignedf32,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-struct Mesh {
-    triangle_count: u32,
-    offset: u32,
-    material: Material
-}
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-struct Vertex {
-    pos: Vec2,
-    color: Vec3,
-}
-
-impl Vertex {
-    const fn new(pos: Vec2, color: Vec3) -> Self {
-        Self { pos, color }
-    }
-
-    fn binding_description() -> vk::VertexInputBindingDescription {
-        vk::VertexInputBindingDescription::builder()
-            .binding(0)
-            .stride(size_of::<Vertex>() as u32)
-            .input_rate(vk::VertexInputRate::VERTEX)
-            .build()
-    }
-
-    fn attribute_descriptions() -> [vk::VertexInputAttributeDescription; 2] {
-        let pos = vk::VertexInputAttributeDescription::builder()
-            .binding(0)
-            .location(0)
-            .format(vk::Format::R32G32_SFLOAT)
-            .offset(0)
-            .build();
-        let color = vk::VertexInputAttributeDescription::builder()
-            .binding(0)
-            .location(1)
-            .format(vk::Format::R32G32B32_SFLOAT)
-            .offset(size_of::<Vec2>() as u32)
-            .build();
-        [pos, color]
     }
 }
