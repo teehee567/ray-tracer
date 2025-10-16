cfe95daa7d49d3f85ee9c8a04213bba4e3ff5bae
Alex <81747218+teehee567@users.noreply.github.com>
2025-10-16 16:38:03 +1100

    add mailbox and change swapchain

--- src/app/constants.rs
+++ src/app/constants.rs
@@ -10,6 +10,7 @@ pub const DEVICE_EXTENSIONS: &[vk::ExtensionName] = &[
     vk::KHR_SHADER_NON_SEMANTIC_INFO_EXTENSION.name,
     vk::EXT_DESCRIPTOR_INDEXING_EXTENSION.name,
     vk::KHR_MAINTENANCE3_EXTENSION.name,
+    vk::KHR_MAINTENANCE1_EXTENSION.name,
 ];
 
 pub const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
